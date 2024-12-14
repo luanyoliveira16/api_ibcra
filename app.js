4a9569b0b0049f6ec17cff60154632bbe49362ce
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 04:13:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como p
 
 const headers = {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'http://localhost:5174',
     'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
 };
 
