81f2490c6a67b7362a3bef8326549a4e400cca54
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 19:59:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   next();
+});
+
 
 app.use(express.json());
 app.use('/membros', router);
