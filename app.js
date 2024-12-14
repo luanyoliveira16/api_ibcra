1af540f535a8a30f36b8b395af27bef08528be5b
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 04:24:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ import router from './routes/membrosRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
-const headers = {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:5174',
-    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-    'Access-Control-Allow-Credentials': true,
-    'Headers': 'Content-Type, Authorization'
-};
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      next();
+    });
 
 
 app.use(cors(headers));
