c4b8baa73863ec7f7440e988eace40a1778fbe27
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 03:00:15 -0300

    cors

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ import router from './routes/membrosRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todos os domínios
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos HTTP permitidos
-    res.setHeader('Access-Control-Allow-Headers', 
-        'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Origin'
-    );
-    next();
-});
+app.use(cors({
+    origin: '*', // Permite todas as origens
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Permite todos os métodos necessários
+    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods'], // Permite os cabeçalhos necessários
+    credentials: true, // Se necessário, permite envio de cookies
+}));
 
 
 app.use(express.json());
