26b587a76cea282a8e501f56fb5f37ce807f38f3
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 04:25:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
     app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Origin', 'http://localhost:5174');
       next();
     });
 
