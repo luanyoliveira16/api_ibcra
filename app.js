dadb6709fab580f04ac373b7e6386189fc7878e0
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 03:31:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como p
 app.use(cors({
     origin:'*',
     methods:['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', "Access-Control-Allow-Origin"]
 }))
 
 app.use(express.json());
