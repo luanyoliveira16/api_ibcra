9a2a52afb8f8058d45d2ec24ebcbfe7cb4a5f914
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 00:21:00 -0300

    cors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import router from './routes/membrosRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json());
 app.use('/membros', router);
 
