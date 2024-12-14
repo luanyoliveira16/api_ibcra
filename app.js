ae36454ce206de36d94ace0bfbd41ddce96358a3
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 04:06:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,21 +6,14 @@ import router from './routes/membrosRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
-const corsOptions = async (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
-
-    // Se for uma solicitação OPTIONS (geralmente usada por navegadores)
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-
-    next();
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS',
 };
 
-app.use(cors(corsOptions));
+
+app.use(cors(headers));
 
 app.use(express.json());
 app.use('/membros', router);
