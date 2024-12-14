05837514e47e3cac66fb85fda7d6437877d96c7f
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 02:34:07 -0300

    cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,17 +1,24 @@
 const allowCors = fn => async (req, res) => {
-          res.setHeader('Access-Control-Allow-Credentials', true);
-          res.setHeader('Access-Control-Allow-Origin', '*'); // Pode ser ajustado para 'http://seu-domínio.com' ou similar
-          res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+          res.setHeader('Access-Control-Allow-Credentials', true)
+          res.setHeader('Access-Control-Allow-Origin', '*')
+          // another common pattern
+          // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+          res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
           res.setHeader(
             'Access-Control-Allow-Headers',
             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-          );
+          )
           if (req.method === 'OPTIONS') {
-            res.status(200).end();
-            return;
+            res.status(200).end()
+            return
           }
-          return await fn(req, res);
-        };
+          return await fn(req, res)
+        }
         
-export default allowCors;
+        const handler = (req, res) => {
+          const d = new Date()
+          res.end(d.toString())
+        }
+        
+        module.exports = allowCors(handler)
         
