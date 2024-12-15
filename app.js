e3086d24b40775450aa7e9efd47faaa44b5f69ab
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-14 21:29:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,23 @@ const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
 
-app.use((req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', '*');
-    res.header('Acess-Control-Allow-Header', 'Content-Type',
-        'Origin, X-Requrested-width, Content-Type, Accept, Authorization'
-    );
 
-    if (req.method === 'OPTIONS') {
-        res.hearder('Acess-Control-Allow-Methods', 'PUT, POST, GET, DELETE')
-        return res.status(200).send({})
-    }
+app.use(cors());
 
-    next();
+// app.use((req, res, next) => {
+//     res.header('Acess-Control-Allow-Origin', '*');
+//     res.header('Acess-Control-Allow-Header', 'Content-Type',
+//         'Origin, X-Requrested-width, Content-Type, Accept, Authorization'
+//     );
 
-});
+//     if (req.method === 'OPTIONS') {
+//         res.hearder('Acess-Control-Allow-Methods', 'PUT, POST, GET, DELETE')
+//         return res.status(200).send({})
+//     }
+
+//     next();
+
+// });
 
 app.use(express.json());
 app.use('/membros', router);
