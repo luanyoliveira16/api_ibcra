74ac4557fa20cbdd206bb8fa25b175b56d67aafa
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 19:42:20 -0300

    Update serverlessVercel.js

--- serverlessVercel.js
+++ serverlessVercel.js
@@ -1,22 +1,23 @@
-const allowCors = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite qualquer origem
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true); // Permite cookies cruzados
-
+  )
   if (req.method === 'OPTIONS') {
-    return res.status(200).end(); // Responde de imediato para preflight requests
+    res.status(200).end()
+    return
   }
-
-  next(); // Continua para o próximo middleware ou rota
-};
+  return await fn(req, res)
+}
 
 const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
+  const d = new Date()
+  res.end(d.toString())
+}
 
-export { allowCors, handler };
+module.exports = allowCors(handler)
