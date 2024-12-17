81f3b213091aea219ea284e59d10d882e3a69bd5
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 18:52:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,51 +6,16 @@ import router from './routes/membrosRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
+// Middleware de CORS
+app.use(cors({
+  origin: '*', // Permite qualquer origem
+  methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'], // Métodos HTTP permitidos
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'], // Cabeçalhos permitidos
+  credentials: true // Permite cookies cruzados
+}));
 
-
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-
-app.use(allowCors);
-
-// app.use((req, res, next) => {
-//     res.header('Acess-Control-Allow-Origin', '*');
-//     res.header('Acess-Control-Allow-Header', 'Content-Type',
-//         'Origin, X-Requrested-width, Content-Type, Accept, Authorization'
-//     );
-
-//     if (req.method === 'OPTIONS') {
-//         res.hearder('Acess-Control-Allow-Methods', 'PUT, POST, GET, DELETE')
-//         return res.status(200).send({})
-//     }
-
-//     next();
-
-// });
-
-app.use(express.json());
-app.use('/membros', router);
+app.use(express.json()); // Middleware para body-parser
+app.use('/membros', router); // Roteamento
 
 app.listen(port, () => {
     console.log(`API rodando na porta ${port}`);
