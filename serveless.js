646dcaa2a75bd049c3ff5aa7568fe592c74649cf
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 20:41:45 -0300

    Update serveless.js

--- serveless.js
+++ serveless.js
@@ -1,47 +1,20 @@
-// Middleware para gerenciar CORS
 const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    res.status(200).end()
+    return
   }
+  return await fn(req, res)
+}
 
-  return await fn(req, res);
-};
+}
 
-// Função para lidar com a requisição
-const handler = async (req, res) => {
-  try {
-    if (req.method === 'GET' && req.url === '/membros') {
-      // Aqui você deve implementar a lógica para buscar os dados dos membros
-      const response = await fetch('https://api-ibcra.vercel.app/membros');
-      if (!response.ok) {
-        throw new Error('Erro ao buscar os membros');
-      }
-      const data = await response.json();
-      
-      res.status(200).json(data);
-    } else if (req.method === 'POST') {
-      // Exemplo de resposta para POST
-      const body = req.body;
-      const data = {
-        message: 'POST request successful',
-        receivedData: body
-      };
-      res.status(200).json(data);
-    } else {
-      // Método não suportado
-      res.setHeader('Allow', ['GET', 'POST']);
-      res.status(405).end(`Método ${req.method} não permitido`);
-    }
-  } catch (error) {
-    // Tratamento de erro genérico
-    res.status(500).json({ error: 'Ocorreu um erro interno' });
-  }
-};
-
-export default allowCors(handler);
+export default allowCors
