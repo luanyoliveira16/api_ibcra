1d8ecc2dc2b15c4d3a94c512277585b010e8951c
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 20:26:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(allowCors)
 
 // Middleware para parsear JSON do corpo da requisição
 app.use(express.json());
