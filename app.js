6046d931051aaf963a92bba26d8dc6dcaf001891
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 19:27:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/membrosRoutes.js';
+import allowCors from './servelessVercerl.js';
 
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
-// Middleware CORS baseado na configuração da Vercel
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite qualquer origem
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  return await fn(req, res);
-};
 
 // Configuração do middleware CORS
 app.use(allowCors); // Adiciona o middleware CORS globalmente
