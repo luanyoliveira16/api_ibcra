f3b3181564196aba232db9475d732ad6334be8de
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 19:29:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,11 @@ const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
 
-// Configuração do middleware CORS
 app.use(allowCors); // Adiciona o middleware CORS globalmente
 
 app.use(express.json()); // Middleware para body-parser
 app.use('/membros', router); // Roteamento
 
-// Handler para endpoints
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
 
 app.use(handler); // Adiciona o handler para lidar com os endpoints
 
