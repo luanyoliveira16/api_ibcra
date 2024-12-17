ec8366e4b28e0c0673c469ccdb9450fd17c42d28
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 18:50:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,31 @@ const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como p
 
 
 
-app.use(cors());
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
+app.use(allowCors);
 
 // app.use((req, res, next) => {
 //     res.header('Acess-Control-Allow-Origin', '*');
