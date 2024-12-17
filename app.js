ff89feb1504a6966977b67797b2cd4e2c0770118
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 19:00:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,32 +6,16 @@ import router from './routes/membrosRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+// Configuração do middleware CORS usando a biblioteca 'cors'
+app.use(cors({
+  origin: '*', // Permite qualquer origem
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Inclui o método DELETE
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Cabeçalhos permitidos
+  credentials: true // Permite cookies cruzados
+}));
 
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-app.use(cors()); // Usado como middleware principal para configuração CORS
 app.use(express.json()); // Middleware para body-parser
 app.use('/membros', router); // Roteamento
-
-// Usando o middleware customizado com CORS
-app.use(allowCors(handler));
 
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`);
