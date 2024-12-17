f8470db7df966a7f9ec7f6df727265280ed2fdcc
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 18:54:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ import router from './routes/membrosRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000; // Usa a porta do ambiente ou 3000 como padrão
 
-// Middleware de CORS
-app.use(cors({
-  origin: '*', // Permite qualquer origem
-  methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'], // Métodos HTTP permitidos
-  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'], // Cabeçalhos permitidos
-  credentials: true // Permite cookies cruzados
-}));
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+app.use(cors()); // Usado como middleware principal para configuração CORS
 app.use(express.json()); // Middleware para body-parser
 app.use('/membros', router); // Roteamento
 
+// Usando o middleware customizado com CORS
+app.use(allowCors(handler));
+
 app.listen(port, () => {
-    console.log(`API rodando na porta ${port}`);
+  console.log(`API rodando na porta ${port}`);
 });
