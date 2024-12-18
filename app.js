96dcaa255382d212aae998cffb42817d38c32e44
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-18 01:04:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const app = express();
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
