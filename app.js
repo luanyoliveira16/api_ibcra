aa97cdfac231517642dcd713a3b6db3587f047e1
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 21:00:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
+
     next();
  })
 
