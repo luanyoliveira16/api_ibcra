e92ac8804e3d367d24da877fd29114cdd81c6216
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 23:21:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-import express from 'express';
-import router from './routes/membrosRoutes.js';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/membrosRoutes.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(cors())
+// Configurações CORS
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+};
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
+app.use(cors(corsOptions));
 
-    // Se for uma requisição do tipo OPTIONS, responde com status 200 imediatamente
-   if (req.method === "OPTIONS") {
-      app.options('*', cors());e
-  }
-
-    next();
-});
-
+// Tratamento para requisições pré-flights (OPTIONS)
 app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
         res.header("Access-Control-Allow-Origin", "*");
@@ -31,16 +26,12 @@ app.use((req, res, next) => {
     }
 });
 
-
-//app.use(cors({origin:true,credentials: true}))
-
 // Middleware para parsear JSON do corpo da requisição
 app.use(express.json());
 
 // Roteamento dos membros
 app.use('/membros', router);
 
-
 app.listen(port, () => {
-  console.log(`API rodando na porta ${port}`);
+    console.log(`API rodando na porta ${port}`);
 });
