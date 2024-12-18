dc4786a04616f3a28f1a0ec5edf9414710c36862
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 23:07:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // app.use(cors())
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
 
-//     // Se for uma requisição do tipo OPTIONS, responde com status 200 imediatamente
-//     if (req.method === 'OPTIONS') {
-//         res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
-//         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//         es.header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
 
-//         return res.status(200).end();
-//     }
+    // Se for uma requisição do tipo OPTIONS, responde com status 200 imediatamente
+    if (req.method === "OPTIONS") {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+    }
 
-//     next();
-// });
+    next();
+});
 
-app.use(cors({origin:true,credentials: true}))
+//app.use(cors({origin:true,credentials: true}))
 
 // Middleware para parsear JSON do corpo da requisição
 app.use(express.json());
