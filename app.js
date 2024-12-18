b2b48ff2c2b8c4d12b73c05e4f41f206c2857380
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2024-12-17 23:03:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use((req, res, next) => {
 
     // Se for uma requisição do tipo OPTIONS, responde com status 200 imediatamente
     if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        es.header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
+
         return res.status(200).end();
     }
 
